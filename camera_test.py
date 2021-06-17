100b86698ba7a77bfe38c383eacde7489d7fc6f7
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2021-06-17 11:14:54 +0800

    ree

--- camera_test.py
+++ camera_test.py
@@ -5,7 +5,7 @@ import argparse
 
 def capture():
 
-	with open("log.txt") as f:
+	with open("log.txt", "w") as f:
 		command = "fswebcam -r 2560x1440 --jpeg 90 -D 4 --no-banner output.jpg"
 
 		process = subprocess.Popen(command.split(" "), stdout = f)

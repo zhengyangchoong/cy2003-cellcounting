632e66c68e554530ac445de00ea88c2f85094780
yz-koh <yzworkk@gmail.com>
2021-07-06 00:21:44 +0800

    Remove redundant min area and add max area cells param

--- contour_detector.py
+++ contour_detector.py
@@ -2,13 +2,14 @@ import cv2
 import numpy as np
 
 
-def contourdetector(image_path, mm_distance = 592):
+def contourdetector(image_path, mm_distance = 592, max_area_cells = 1500):
   """
   preprocesses image, performs Otsu's thresholding and connected components detection to derive cell count by hemocytometer method
   excludes cells on outermost bottom and right grid lines
 
   :param      image_path:  The image file path to jpg
   :param      mm_distance: number of pixels per mm
+  :param      max_area_cells: maximum enclosed area that contour detector will accept for it to count as a cell
 
   returns an integer as the cell count of the image
   """
@@ -34,13 +35,11 @@ def contourdetector(image_path, mm_distance = 592):
   cnts, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
   cnts = [cnts[i] for i in range(len(cnts)) if hierarchy[0][i][2] == -1] # count contours with no child contours only
 
-
-  min_area = 0 #set minimum area to 0 as dots are vv small and preprocessing makes sure cell nuclei is brightest
   white_dots = [] # used contours as blob detection is more suited for detecting black or grey blobs
 
   for c in cnts:
       area = cv2.contourArea(c)
-      if 1500 > area > min_area:
+      if max_area_cells > area :
           cv2.drawContours(image, [c], -1, (36, 255, 12), 2)
           white_dots.append(c)
 

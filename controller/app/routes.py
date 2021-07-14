43e1fb1216bce2441c3abbb5501adc4bf7ee683e
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2021-07-14 10:48:44 +0800

    test home

--- controller/app/routes.py
+++ controller/app/routes.py
@@ -156,7 +156,7 @@ class MicroscopeController():
 		if self.internal_state["move_direction"] == "-":
 			distance *= -1
 
-		self.simple_move(axis = self.intern_state["move_axis"])
+		self.simple_move(axis = self.internal_state["move_axis"])
 
 
 	def move(self, abs_x = 0, abs_y = 0, abs_z = 0):

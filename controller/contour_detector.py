7a2dfca8110f4a0ff9e14683874dba2b03e46f28
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2021-07-21 10:46:40 +0800

    offline

--- controller/contour_detector.py
+++ controller/contour_detector.py
@@ -295,6 +295,6 @@ def v2_contourdetector(image_path, mm_distance = 592, max_area_cells = 800):
 
   output_path = "app/static/capture/{}.jpg".format(datetime.datetime.now(), "%Y%m%d-%H%M%S")
 
-  cv2.imwrite(output_path,image)
+  cv2.imwrite(output_path,thresh)
 
   return len(white_dots), output_path # returns cell count and image with drawn contours
